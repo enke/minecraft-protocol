3976e31a57ed6797658f26bbcd0e7d70d8325889
enke <565565+enke@users.noreply.github.com>
2018-02-04 14:10:24 +0300

    Allow to not set online players

--- src/main/kotlin/ru/enke/minecraft/protocol/packet/data/status/ServerStatusInfo.kt
+++ src/main/kotlin/ru/enke/minecraft/protocol/packet/data/status/ServerStatusInfo.kt
@@ -5,5 +5,5 @@ import java.util.*
 
 data class ServerStatusInfo @JvmOverloads constructor(val version: Version, val description: Message, val players: Players, val favicon: String? = null)
 data class Version(val name: String, val protocol: Int)
-data class Players(val online: Int, val max: Int, val list: List<Player>)
+data class Players @JvmOverloads constructor(val online: Int, val max: Int, val list: List<Player> = Collections.emptyList())
 data class Player(val id: UUID, val name: String)
